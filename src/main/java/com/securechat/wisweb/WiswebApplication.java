cb1393cb104ab97198245940095ae35876ce5c18
Aleks C <useruser@192.168.1.103>
2024-12-11 14:07:46 +0300

    Generated Spring Project with dependencies

---
+++ src/main/java/com/securechat/wisweb/WiswebApplication.java
@@ -0,0 +1,13 @@
+package com.securechat.wisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securechat/wisweb/WiswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securechat.wisweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
