3e15207fa95f9da49d37885e0665634c66cc3939
Aleks C <useruser@Users-MacBook-Pro.local>
2024-12-17 16:29:22 +0300

    Countdown added on the Chat page

--- src/main/java/com/securechat/wisweb/model/ChatRoom.java
+++ src/main/java/com/securechat/wisweb/model/ChatRoom.java
@@ -4,14 +4,18 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 public class ChatRoom {
     private final String id;
     private String oneTimeToken;
+@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private final LocalDateTime creationTime;
     private boolean isActive;
     private String hostNickname;
     private String guestNickname;
+    
 
     public ChatRoom(String hostNickname) {
         this.id = UUID.randomUUID().toString();
